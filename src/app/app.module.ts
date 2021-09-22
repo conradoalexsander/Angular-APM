c51d10a15bd4912140f6eabb9103a8a6b90e6267
conrado.goncalves <53683786+conradoalexsander@users.noreply.github.com>
2021-09-22 08:57:54 +0200

    Product List component

---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/productlist.components';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductListComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
