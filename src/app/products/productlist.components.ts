7e913c16b952c77c4ebcfa577d80ddbb12bcfb5b
conrado.goncalves <53683786+conradoalexsander@users.noreply.github.com>
2021-09-28 19:58:44 +0200

    Added star rating

--- src/app/products/productlist.components.ts
+++ src/app/products/productlist.components.ts
@@ -1,16 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './product';
 
 @Component({
     selector: 'pm-products',
-    templateUrl: './product-list.component.html'
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number = 50;
     showImage: boolean = false;
-    listFilter: string = 'cart';
-    products: any[] = [
+
+
+    private _listFilter: string = '';
+    get listfilter(): string {
+        return this._listFilter;
+    }
+    set listFilter(value: string) {
+        this._listFilter = value;
+        this.filteredProducts = this.performFilter(value);
+    }
+
+    filteredProducts: IProduct[] = [];
+    products: IProduct[] = [
         {
             "productId": 1,
             "productName": "Leaf Rake",
@@ -65,5 +78,14 @@ export class ProductListComponent {
 
     toggleImage(): void {
         this.showImage = !this.showImage;
+    }
+
+    performFilter(filterBy: string): IProduct[] {
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter((product: IProduct) => product.productName.toLocaleLowerCase().includes(filterBy));
+    }
+
+    ngOnInit(): void {
+        this.listFilter = '';
     }
 }
