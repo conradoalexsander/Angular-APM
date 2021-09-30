c68e1c921166ecdfc355a08b3a856077c96f2abf
conrado.goncalves <53683786+conradoalexsander@users.noreply.github.com>
2021-09-30 12:42:35 +0200

    Added dynamic product to product detail

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,30 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css']
 })
-export class ProductDetailComponent implements OnInit {
+export class ProductDetailComponent implements OnInit, OnDestroy {
+  constructor(private productService: ProductService, private route: ActivatedRoute, private router: Router) { }
   pageTitle: string = 'Product Detail';
   product: IProduct | undefined;
-
-  constructor(private route: ActivatedRoute, private router: Router) { }
+  sub!: Subscription;
 
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
     this.pageTitle += `: ${id}`;
-    this.product = {
-      "productId": 2,
-      "productName": "Garden Cart",
-      "productCode": "GDN-0023",
-      "releaseDate": "March 18, 2021",
-      "description": "15 gallon capacity rolling garden cart",
-      "price": 32.99,
-      "starRating": 4.2,
-      "imageUrl": "assets/images/garden_cart.png"
-    };
+    this.sub = this.productService.getProducts().subscribe({
+      next: products => {
+        this.product = products.find(product => product.productId == id);
+      },
+      error: err => console.log('Invalid product id')
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe;
   }
 
   onBack(): void {
